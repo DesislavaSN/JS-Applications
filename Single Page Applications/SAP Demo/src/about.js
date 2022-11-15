f8f2e7043a17dcf54a11eed3d1adf15f8043a552
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-11-15 16:38:01 +0000

    Delete Single Page Applications/SAP Demo directory

--- Single Page Applications/SAP Demo/src/about.js
+++
@@ -1,7 +0,0 @@
-const section = document.getElementById('aboutView');
-section.remove();
-
-export function showAbout(){
-    document.querySelector('main').replaceChildren(section);
-
-}
--- Single Page Applications/SAP Demo/src/home.js
+++
@@ -1,6 +0,0 @@
-const section = document.getElementById('homeView');
-section.remove();
-
-export function showHome(){
-    document.querySelector('main').replaceChildren(section)
-}
