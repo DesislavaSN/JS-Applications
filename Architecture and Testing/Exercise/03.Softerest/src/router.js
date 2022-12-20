d98558ef0251288d57f3bc7fc96db952599e53b3
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2022-12-20 12:39:14 +0000

    Update router.js

--- Architecture and Testing/Exercise/03.Softerest/src/router.js
+++ Architecture and Testing/Exercise/03.Softerest/src/router.js
@@ -1,16 +1,8 @@
-/* Navigator page - f. intialize priema 'links' ( koito sa v app.js) za da moje da gi dosupi v tozi file. 
-Router-a podava sebe si v handler-a (na 40ti red) i vsichki shte mogat da dostupqt 
-2te f. showSection & goTo */
-
 export function initialize(links) {
-  // suzdadohme si main tag v HTML file za da zakachame po otdelno vsqko View:
   const main = document.querySelector("main");
-  /*hvashtame NAV bar-a i my zakachame event da slysha za click na opredelen A tag*/
   const nav = document.querySelector("nav");
   nav.addEventListener("click", onNavigate);
 
-
-  /* router-a shte suzdade 'context' i shte go vurne */
   const context = {
     showSection,
     goTo,
@@ -18,16 +10,11 @@ export function initialize(links) {
   };
   return context;
 
-
-  /*suzdavame si f. koqto vizyalizira sekciite */
   function showSection(section) {
     main.replaceChildren(section);
   }
 
-  // click handler - navigira po NAV bar-a
   function onNavigate(event) {
-    /*tui kato v linka za homePage e image to 
-    redirektvame kum parent element-a koito e 'A' tag */
     if (event.target.tagName == "IMG") {
       event.target = event.target.parentElement;
     }
@@ -38,17 +25,13 @@ export function initialize(links) {
     }
   }
 
-  /*namira po ime v dadenite linkove kakvo se opitvame da vizyalizirame;
-  '...params' - oznachava hvani vsichki parametri sled 'name' */ 
   function goTo(name, ...params) {
     const handler = links[name];
     if (typeof handler == "function") {
-      /* ...params - oznachava vsichki parametri gi razdeli na otdelni elementi */
       handler(context, ...params);
     }
   }
 
-  /*f. kakvo vijda v NAV bar-a logged-nat potrebitel ili guest bez registraciq */
   function updateNav(){
     const user = localStorage.getItem('user');
     if (user) {
